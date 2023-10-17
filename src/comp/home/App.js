27d7bb279f5d52055e796db4ade51dba15ca96dd
Mohamed MRI <mohamedmri.21@uom.lk>
2023-10-18 00:30:25 +0530

    the unwanted movie1 object removed

--- src/comp/home/App.js
+++ src/comp/home/App.js
@@ -5,15 +5,6 @@ import MovieCard from "../sub/MovieCard";
 
 const API_URl = "http://www.omdbapi.com?apikey=8a41cac5";
 
-const movie1 = {
-  Title: "Italian Spiderman",
-  Year: "2007",
-  imdbID: "tt2705436",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -32,7 +23,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>Movie Master</h1>
 
       <div className="search">
         <input
